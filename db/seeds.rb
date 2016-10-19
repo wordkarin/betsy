5e83b3d0a2d7e04a4e84abdde37c9d7aba40baa4
Madeleine Johnson <mjohnson3038@gmail.com>
2016-10-19 15:28:40 -0700

    OrderItem and ProductCategory Seed Files

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,16 @@ CSV.read("seed_csvs/products.csv").each do |t|
   Product.create(name: t[0], price: t[1], stock_quantity: t[2], description: t[3], photo_url: t[4], merchant_id: t[5])
 end
 
+# Order_Item
+CSV.read("seed_csvs/order_items.csv").each do |t|
+  OrderItem.create(order_id: t[0], product_id: t[1], quantity: t[2])
+end
+
+#Product_Category
+CSV.read("seed_csvs/product_categories.csv").each do |t|
+  ProductCategory.create(product_id: t[0], category_id: t[1])
+end
+
 # Merchant seed
 CSV.read("seed_csvs/merchant.csv").each do |line|
   id, name, email = line # parallel assignment!
