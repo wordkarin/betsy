3747b955fb028abbee1c817777f5eddbb5213926
LaurenSky <ljherrer@gmail.com>
2016-10-20 21:17:17 -0700

    some Review model validation tests completed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+  
+  validates :rating, presence: true, :numericality => {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Rating must be present
+  # Rating must be between 1 and 5
+  test "Creates a review with a rating that is an integer" do
+    review = Review.new(rating: 4)
+    assert review.valid?
+  end
+
+  test "Cannot create a review without a rating" do
+    review = Review.new(title: "Awesome Product")
+    assert_not review.valid?
+  end
+
+  # Rating must be an integer
+  test "Cannot create a review with a rating that is not and integer" do
+    review = Review.new(rating: "five stars")
+    assert_not review.valid?
+  end
+
+  # Rating must be between 1 and 5
+  test "Cannot create a review with a rating that is > 5" do
+    review = Review.new(rating: 6)
+    assert_not review.valid?
+  end
+
+  test "Cannot create a review with a rating that is < 1" do
+    review = Review.new(rating: 0)
+    assert_not review.valid?
+  end
+
+  
 end
