19a4e81dbfd427ddbc8a8da323e72580777435c5
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-20 17:00:14 -0700

    added validations and stubs for actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  def index
+    #shows all the products in the site
+  end
+
+  def edit
+    # This is the page where a merchant can edit their own product, will have an authorization to make sure merchant's id matches product's merchant_id
+  end
+
+  def show
+    # This is the individual product page
+  end
+
+  def update
+    # This is the PATCH call when a merchant updates their product
+  end
+
+  def retired
+    # This is the PATCH call when a merchant retires a product
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,11 @@ class Product < ActiveRecord::Base
 
   has_many :product_categories
   has_many :categories, :through => :product_categories
+
+  validates :name, presence: true, uniqueness: true
+
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # I think that we should make sure that a product has at least one category? We can discuss. 
+  validates :categories, :length => { :minimum => 1 }
 end
