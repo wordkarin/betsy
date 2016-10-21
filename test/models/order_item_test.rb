ed82f466e77a4f2f4fb8ae94892218a29fff4c8b
Madeleine Johnson <mjohnson3038@gmail.com>
2016-10-21 08:51:04 -0700

    Are the product_id and order_id valid?

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -3,22 +3,36 @@ require 'test_helper'
 class OrderItemTest < ActiveSupport::TestCase
   test "Must belong to a product, ie a valid product id" do
     order_item = OrderItem.new(order_id: 1, quantity: 2)
+    order_item = OrderItem.new(order_id: 1, product_id: "a", quantity: 2)
     assert_not order_item.valid?
   end
 
-  # test "Must belong to a valid product" do
-  #   product = Product.new(id: 1 ,name: "Cheese", price: 670)
-  #   product.save
-  #   # may need to include user_id when combining tests, but here it is said to be invalid
-  #   order_item = OrderItem.new(product_id: 1, order_id: 1, quantity: 2)
-  #   order_item.save
-  #   assert_includes Product.all, Product.find(order_item.order_id)
-  # end
+  # I feel that making sure that the OrderItem is associated with a valid product_id and order_id. I think it might be helpful to talk about if this is useful. 
+  test "Must belong to a valid product" do
+    order_item = OrderItem.new(product_id: 1, order_id: 1, quantity: 2)
+    order_item2 = OrderItem.new(product_id: 4, order_id: 1, quantity: 2)
+    order_item.save
+    order_item2.save
+    assert_includes Product.all, Product.where(order_item.order_id).first
+    assert_not Product.all.pluck(:id).include? Product.where(order_item2.order_id).first
+  end
 
-  test "Must belong to an object, ie a valid object id" do
+  test "Must belong to an Order, ie a valid order id" do
     order_item = OrderItem.new(product_id: 1, quantity: 2)
     assert_not order_item.valid?
   end
+
+  # test "Must belong to a valid order" do
+  #   order = Order.new(id: 4)
+  #   order.save
+  #   # may need to include user_id when combining tests, but here it is said to be invalid
+  #   order_item = OrderItem.new(product_id: 1, order_id: 4, quantity: 2)
+  #   order_item.save
+  #   order_item2 = OrderItem.new(product_id: 1, order_id: 3, quantity: 2)
+  #   order_item2.save
+  #   assert_includes Order.all, Order.where(order_item.order_id).first
+  #   assert_empty Order.where(order_item2.order_id)
+  # end
 
   test "Should reject an OrderItem with no quantity" do
     order_item = OrderItem.new(order_id: 1, product_id: 1)
