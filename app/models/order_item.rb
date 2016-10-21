d7965a86a1fd68c361f7b6c5656add05a4b29136
Madeleine Johnson <mjohnson3038@gmail.com>
2016-10-21 08:11:58 -0700

    OrderItem Tests and Validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,7 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :product_id, presence: true
+  validates :order_id, presence: true
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "Must belong to a product, ie a valid product id" do
+    order_item = OrderItem.new(order_id: 1, quantity: 2)
+    assert_not order_item.valid?
+  end
+
+  # test "Must belong to a valid product" do
+  #   product = Product.new(id: 1 ,name: "Cheese", price: 670)
+  #   product.save
+  #   # may need to include user_id when combining tests, but here it is said to be invalid
+  #   order_item = OrderItem.new(product_id: 1, order_id: 1, quantity: 2)
+  #   order_item.save
+  #   assert_includes Product.all, Product.find(order_item.order_id)
   # end
+
+  test "Must belong to an object, ie a valid object id" do
+    order_item = OrderItem.new(product_id: 1, quantity: 2)
+    assert_not order_item.valid?
+  end
+
+  test "Should reject an OrderItem with no quantity" do
+    order_item = OrderItem.new(order_id: 1, product_id: 1)
+    assert_not order_item.valid?
+  end
+
+  test "Should reject an OrderItem with a quantity other than an integer" do
+    order_item = OrderItem.new(order_id: 1, product_id: 1, quantity: "a")
+    order_item2 = OrderItem.new(order_id: 1, product_id: 1, quantity: false)
+    assert_not order_item.valid?
+    assert_not order_item2.valid?
+  end
+
+  test "Should reject an OrderItem with a quantity less than or equal to 0" do
+    order_item = OrderItem.new(order_id: 1, product_id: 1, quantity: -1)
+    order_item2 = OrderItem.new(order_id: 1, product_id: 1, quantity: 0)
+    assert_not order_item.valid?
+    assert_not order_item2.valid?
+  end
 end
