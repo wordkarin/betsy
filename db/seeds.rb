e3ca2eac9f37329a13ee56a485273e1b6aaa86f6
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-21 15:06:52 -0700

    fixed the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# We have validations for order requires that at least one order_item exists (and we do have the csv's set up correctly, but the order matters and fails to seed if we do them separately).
+# We have validations on product that requires product_categories to exist (which also means that the csv's which are set up correctly, but can't be seeded independently.) Right now we're removing the validations so that the database seeds. If we have time we're going to consolidate them. 
 # Possibly use create! if we want to get an error on validations for our seed data, or we could turn off validations if we know that our seed data is valid.
 
 require 'csv'
@@ -9,7 +11,7 @@ CSV.read("seed_csvs/merchant.csv").each do |line|
 
 # This is how you would save without using validations
   merchant.save(validate: false)
-# Should maybe check here that all of our records got added. 
+# Should maybe check here that all of our records got added.
 end
 
 CSV.foreach('./seed_csvs/categories.csv', :headers => true) do |row|
@@ -17,11 +19,13 @@ CSV.foreach('./seed_csvs/categories.csv', :headers => true) do |row|
 end
 
 CSV.read("seed_csvs/products.csv").each do |t|
-  Product.create!(name: t[0], price: t[1], stock_quantity: t[2], description: t[3], photo_url: t[4], merchant_id: t[5])
+  product = Product.new(name: t[0], price: t[1], stock_quantity: t[2], description: t[3], photo_url: t[4], merchant_id: t[5])
+  product.save(validate:false)
 end
 
 CSV.foreach('./seed_csvs/orders.csv', :headers => true) do |row|
-    Order.create(row.to_hash)
+    order = Order.create(row.to_hash)
+    order.save(validate:false)
 end
 
 CSV.foreach('./seed_csvs/reviews.csv', :headers => true) do |row|
