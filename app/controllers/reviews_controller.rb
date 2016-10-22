2dffd0f515c66cb731028d52af5938ffac4f323f
LaurenSky <ljherrer@gmail.com>
2016-10-21 22:15:52 -0700

    added reviews#index method and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews_by_product = Review.where(product_id: params[:product_id])
+    @product = @reviews_by_product.first.product
+  end
+
+  def show
+
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to product_reviews_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :title, :body)
+  end
 end
