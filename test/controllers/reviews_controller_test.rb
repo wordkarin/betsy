3667654701364daf3ae5db32484cc407b03bb6cc
LaurenSky <ljherrer@gmail.com>
2016-10-23 21:53:13 -0700

    added foundation to project

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def login_a_user
+  request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+  get :create,  {provider: "google"}
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MerchantTest < ActiveSupport::TestCase
 
   test "Created merchant can have a name and an email" do
-    merchant = Merchant.create!(name: "Gingham Suppliers", email: "gingham@suppliers.ya")
+    merchant = Merchant.create!(name: "Gingham Suppliers", email: "gingham@suppliers.ya", uid: "1234", provider: 'google')
     assert merchant.valid?
   end
 
@@ -13,18 +13,18 @@ class MerchantTest < ActiveSupport::TestCase
   end
 
   test "Merchants must be created with unique name and email must be unique" do
-    valid_merchant = Merchant.create!(name: "Gingham Suppliers", email: "gingham@suppliers.ya")
-    invalid_merchant = Merchant.new(name: "Gingham Suppliers", email: "gingham@suppliers.ya")
-    merchant_invalid_name = Merchant.new(name: "Gingham Suppliers", email: "picnic@suppliers.ya")
-    merchant_invalid_email = Merchant.new(name: "Picnic Suppliers", email: "gingham@suppliers.ya")
+    valid_merchant = Merchant.create!(name: "Gingham Suppliers", email: "gingham@suppliers.ya", uid: "3333", provider: 'google')
+    invalid_merchant = Merchant.new(name: "Gingham Suppliers", email: "gingham@suppliers.ya", uid: "2222", provider: 'google')
+    merchant_invalid_name = Merchant.new(name: "Gingham Suppliers", email: "picnic@suppliers.ya", uid: "1111", provider: 'google')
+    merchant_invalid_email = Merchant.new(name: "Picnic Suppliers", email: "gingham@suppliers.ya", uid: "2345", provider: 'google')
     assert_not invalid_merchant.valid?
     assert_not merchant_invalid_name.valid?
     assert_not merchant_invalid_email.valid?
   end
 
   test "Many merchants can be saved on the database, and be individually retrieved" do
-    merchant = Merchant.new(name: "Gingham Suppliers", email: "gingham@suppliers.ya")
-    merchant2 = Merchant.new(name: "Strawberries and Champagne", email: "little.shop@strawberries.fr")
+    merchant = Merchant.new(name: "Gingham Suppliers", email: "gingham@suppliers.ya", uid: "5555", provider: 'google')
+    merchant2 = Merchant.new(name: "Strawberries and Champagne", email: "little.shop@strawberries.fr", uid: "7777", provider: 'google')
 
     assert merchant.save
     assert merchant2.save
