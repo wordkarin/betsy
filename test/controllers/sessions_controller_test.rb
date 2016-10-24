2f209cb02e3b84a5de0273fd1016d0f4844bb364
LaurenSky <ljherrer@gmail.com>
2016-10-23 20:41:50 -0700

    completed sessions tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -13,7 +13,7 @@ class SessionsControllerTest < ActionController::TestCase
     assert_no_difference('Merchant.count') do
       get :create,  {provider: "google"}
     end
-    assert_redirected_to sessions_path
+    assert_redirected_to sessions_path  #IS THIS SUPPOSED TO BE THE SESSIONS PATH OR THE ROOT_PATH?????
   end
 
   test "Can Create a user" do
@@ -25,26 +25,26 @@ class SessionsControllerTest < ActionController::TestCase
       assert_not_nil session[:user_id]
     end
   end
-  #
-  # test "If a user logs in twice it doesn't create a 2nd user" do
-  #   assert_difference('Merchant.count', 1) do
-  #     login_a_user
-  #   end
-  #
-  #   assert_no_difference('Merchant.count') do
-  #     login_a_user
-  #     assert_response :redirect
-  #     assert_redirected_to sessions_path
-  #     assert_not_nil session[:user_id]
-  #   end
-  # end
-  #
-  # test "If a user logs out user_id should be nil and should be redirected to the right place" do
-  #   login_a_user
-  #   get :destroy
-  #
-  #   assert_response :redirect
-  #   assert_redirected_to root_path
-  #   assert_nil session[:user_id]
-  # end
+
+  test "If a user logs in twice it doesn't create a 2nd user" do
+    assert_difference('Merchant.count', 1) do
+      login_a_user
+    end
+
+    assert_no_difference('Merchant.count') do
+      login_a_user
+      assert_response :redirect
+      assert_redirected_to sessions_path
+      assert_not_nil session[:user_id]
+    end
+  end
+
+  test "If a user logs out user_id should be nil and should be redirected to the right place" do
+    login_a_user
+    get :destroy
+
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_nil session[:user_id]
+  end
 end
