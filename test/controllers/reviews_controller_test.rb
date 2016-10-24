5115f4e1b0fd6737b9d39a1b110d4348959c4cf7
LaurenSky <ljherrer@gmail.com>
2016-10-24 11:14:09 -0700

    playing with styles more

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
-  def login_a_user
-  request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-  get :create,  {provider: "google"}
-  end
+  # def login_a_user
+  # request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+  # get :create,  {provider: "google"}
+  # end
 
   test "should get index" do
+    
     get :index
     assert_response :success
   end
