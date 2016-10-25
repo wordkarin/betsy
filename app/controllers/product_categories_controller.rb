153ccfab2c07cebd72ac3852b50f5917c60a2e62
LaurenSky <ljherrer@gmail.com>
2016-10-25 12:56:54 -0700

    added prodcut_category_params method to private in controller-- not completed so commented out

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -5,12 +5,20 @@ class ProductCategoriesController < ApplicationController
   end
 
   def create
-
+    raise
   end
 
   def destroy
   end
 
+  private
 
-  
+  # def product_category_params
+  #   params.require(:product_category).permit(
+  #
+  #   )
+  #
+  # end
+
+
 end
