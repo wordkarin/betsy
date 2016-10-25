83bf468b535d4cc230d236398f8bfefd0a3a56b9
Madeleine Johnson <mjohnson3038@gmail.com>
2016-10-25 12:28:21 -0700

    REALIZATION - we don't need an OI Controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,14 +12,18 @@ class OrdersController < ApplicationController
       render product_path(@product.id)
     end
   end
-  # def show
-  #   @order = Order.find(params[:id].to_i)
-  # end
+
+  def show
+    @order = Order.find(params[:id].to_i)
+    @order_items = OrderItem.where(order_id = params[:id].to_i)
+  end
   #
   # def edit
   #   @order = Order.find(params[:id].to_i)
   #   # needs to be able to select the order_item
   # end
+
+
   private
   def order_params(params)
     # params.require(:order).permit(:name, :email, :mailing_address, :cc_last_4, :cc_expire, :status)
