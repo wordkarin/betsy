5b6a2e925fc3a5116f48cbec877a69061cad69b0
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-26 15:09:58 -0700

    order_item create method updated

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,4 +5,15 @@ class OrderItem < ActiveRecord::Base
   validates :product, presence: true
   # validates_inclusion_of :product_id, :in => Product.all.pluck(:id)
   validates :order, presence: true
+
+  def self.create_order_item(product, order)
+    order_item = product.order_items.new(quantity: 1)
+    order_item.order_id = order.id
+    if order_item.save
+      return order_item
+    else
+      return false
+    end
+
+  end
 end
