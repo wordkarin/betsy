9052443dc3d9add8f92999fd86adbe22cb27d692
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-26 15:56:04 -0700

    logged out guests and wrong merchants cannot edit products from merchant page

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,7 +4,15 @@ class MerchantsController < ApplicationController
   end
 
   def show
+    current_user
     @merchant = Merchant.find(params[:id])
+
+    if @current_user == nil || @current_user != @merchant
+      @user_page = false
+    else
+      @user_page = true
+    end
+
   end
 
 end
